d8a2783bb1044fe7a2cea5cad6506919f3504d54
Francisco Estrada <fjestrada3@utpl.edu.ec>
2022-01-21 11:42:48 -0500

    primer commit

--- Ejemplo/src/ejemplo/Ejemplo.java
+++ Ejemplo/src/ejemplo/Ejemplo.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ejemplo;
-
+import org.apache.commons.lang3.RandomUtils;
 /**
  *
  * @author reroes
@@ -15,7 +15,25 @@ public class Ejemplo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int numeroInicial = 100;
+        int numeroFinal = 110;
+        
+        int[] datos = obtenerNumero(numeroInicial,numeroFinal);
+        
+        for(int i = 0; i<datos.length;i++){
+            System.out.printf("%d\n",datos[i]);
+        }
+        
+        
     }
-    
+    public static int[] obtenerNumero(int a,int b){
+        int[] numeros = new int[10];
+        
+        for(int i=0;i<numeros.length;i++){
+            numeros[i] = RandomUtils.nextInt(a, b);
+        }
+        
+        return numeros;
+}
 }
